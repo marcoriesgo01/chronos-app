af108d6609e26ac439b0f458aa85e7dfe861267d
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-06-05 16:40:40 -0700

    post review form working perfectly

--- client/src/components/categories/Profile.js
+++ client/src/components/categories/Profile.js
@@ -12,7 +12,11 @@ class CategoryDashboard extends Component {
   state = {
     currentCategory: [],
     userProfile: [],
-    userReviews: []
+    userReviews: [],
+    createReview: false,
+    editReview: false,
+    rating: 4,
+    comment: ""
   }
 
   componentDidMount() {
@@ -22,6 +26,10 @@ class CategoryDashboard extends Component {
     this.getUserReviews();
     // this.getCategoryUsers();
   }
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
 
   getUserReviews = () =>  {
     const {user} = this.props.location.state;
@@ -41,7 +49,7 @@ class CategoryDashboard extends Component {
   }
 
   //delete a review if its written by you
-  deleteBudget = id => {
+  deleteReview = id => {
     fetch('/api/reviews/' + id, {
       method: 'DELETE'
     }).then( res => {
@@ -53,19 +61,59 @@ class CategoryDashboard extends Component {
       })
     }).then(this.getUserReviews())
   }
+
+  //Form Conditionals with state:
+  handleOpenCreateReview = () => {
+    this.setState({createReview: true, editReview: false})
+  }
+
+  handleOpenEditReview = () => {
+    this.setState({createReview: false, editReview: true})
+  }
+
+  handleCloseReviewForms = () => {
+    this.setState({createReview: false, editReview: false})
+  }
+
+  //submit review
+  handleReviewSubmit = (event) => {
+    event.preventDefault()
+    const {user} = this.props.location.state;
+    fetch('/api/reviews/create',{
+    body: JSON.stringify({
+      author: this.props.auth.user.id,
+      authorName: this.props.auth.user.name,
+      rating: this.state.rating,
+      reviewedUser: user._id,
+      comment: this.state.comment
+    }),
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => {
+    this.setState({
+        rating: 0,
+        comment: ""
+    })
+  })
+  .then(this.handleCloseReviewForms)
+  .then(this.getUserReviews)
+  .catch(error => console.error({ Error: error }));
+}
   
   render() {
 
-    console.log(this.props.auth.user);
+    // console.log(this.props.auth.user);
     const {category} = this.props.location.state;
     const {user} = this.props.location.state;
     // console.log(category)
     // console.log(this.props.auth.user)
     // console.log(category)
-    // console.log(user)
     // console.log(this.state.userProfile)
     // console.log(this.state.userReviews)
-    
     
     return (
       <div id="category-main-container">
@@ -121,10 +169,40 @@ class CategoryDashboard extends Component {
                 <h4 id="message-board-title">{user.name.split(" ")[0]}'s Reviews</h4>
               </div>
               <div className="user-reviews-title-button-container">
-                <button onClick={this.onLogoutClick} className="btn waves-effect waves-light hoverable" id="create-review-button">
+                <button onClick={this.handleOpenCreateReview} className="btn waves-effect waves-light hoverable" id="create-review-button">
                   Post a Review
                 </button>
               </div>
+              { this.state.createReview ?
+                <div className="row">
+                  <form className="col s12" onSubmit={this.handleReviewSubmit}>
+                    <div className="row">
+                      <div class="input-field col s1">
+                        <select class="browser-default" 
+                          onChange={this.handleChange}
+                          id="rating"
+                          name="rating"
+                          type="number">
+                          <option value="" disabled selected>Rating</option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                        </select>
+                      </div>
+                      <div className="input-field col s6">
+                        <input type="text" id="comment" name="comment" className="validate" onChange={this.handleChange} />
+                        <label htmlFor="amount">Review</label>
+                      </div>
+                      <input type="submit" className="btn" id="budget-form-button" value="Enter"/>
+                      <button onClick={this.handleCloseReviewForms} className="btn" id="budget-form-cancel-button">
+                        Cancel
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              :null }
               { this.state.userReviews.length < 1 ?
                 <div className="no-user-reviews-container">
                   <h5 className="no-user-reviews-text-line-one">{user.name.split(" ")[0]} has no reviews yet.</h5>
@@ -155,10 +233,15 @@ class CategoryDashboard extends Component {
                               </div>
                               <div className="review-item-bottom-content">
                                 <div className="review-item-bottom-left-content">
-                                  <h5>Comment: {review.comment}</h5>
+                                  <h5>{review.comment}</h5>
                                 </div>
                                 <div className="review-item-bottom-right-content">
-                                  <h5>Button</h5>
+                                  <button onClick={() => this.deleteReview(review._id)} className="btn btn-small btn-floating waves-effect waves-light hoverable" id="edit-review-button">
+                                    <i className="material-icons">mode_edit</i>
+                                  </button>
+                                  <button onClick={() => this.deleteReview(review._id)} className="btn btn-small btn-floating waves-effect waves-light hoverable" id="trash-review-button">
+                                    <i className="material-icons">delete</i>
+                                  </button>
                                 </div>
                               </div>
                           </div>
