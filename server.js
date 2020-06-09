8f9ef59dc90300e990a19bd22a97ef218df33514
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-06-09 12:29:48 -0700

    going to deploy with youtubedemo

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const port = process.env.PORT || 5000 // process.env.port is Heroku's port if yo
 // DB Config
 const db = require("./config/keys").mongoURI;
 
-const MONGODB_URI = process.env.MONGODB_URI || db
+// const MONGODB_URI = process.env.MONGODB_URI || db
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || db, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const connected = mongoose.connection;
 
@@ -41,20 +41,17 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 
-// Routes
-app.use("/api/users", users);
-app.use("/api/categories", categories);
-app.use("/api/reviews", reviews);
 
 //Server static assts if in production:
 if (process.env.NODE_ENV === 'production') { 
   //set static folder
   app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
 }
+
+// Routes
+app.use("/api/users", users);
+app.use("/api/categories", categories);
+app.use("/api/reviews", reviews);
 
 
 app.listen(port, () => console.log("Server running on port:", port));
