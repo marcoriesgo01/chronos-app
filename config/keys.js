cf29a7c4575cd349d2672337a59907823b25c2e5
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-06-09 12:06:49 -0700

    deleted heroku folder

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://mriesgo:Pacama2001@cluster0-vapr6.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://mriesgo:Pacama2001@chronos.vapr6.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
